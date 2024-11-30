3b636db2237b1f4880478bd5a36783965d87a2d1
JJ Merelo <jjmerelo@gmail.com>
2024-11-30 18:58:58 +0100

    Checking files

--- action.src.pl
+++ action.src.pl
@@ -6,15 +6,25 @@ use feature 'signatures';
 use lib qw(lib);
 
 use Markdowner qw(headerOK);
+use Github::Actions;
+
 
 my @directories = split(" ", $ENV{'DIRS'});
 
+start_group;
 for my $dir (@directories) {
   my @markdownFiles = glob("$dir/*.md");
   for my $mdFile (@markdownFiles) {
     open my $mdfh, "<", $mdFile;
     my $firstLine = <$mdfh>;
-    say $firstLine;
+    close my $mdfh;
+    chop( $firstLine );
+    if ( headerOK( $firstLine ) ) {
+      debug "«$firstLine» is proper markdown ho, ho, ho";
+    } else {
+      error_on_file( "Haar! «$firstLine» is not proper markdown", $mdFile, 1, 1 );
+    }
   }
 }
+end_group;
 
